6453bd254a18e0336fb6616107239d54b36d9cdf
ygor-salles <ygorsallesaniceto@gmail.com>
2021-07-16 14:35:22 -0300

    Finalizando estilização front

--- frontend-react/src/components/Home.tsx
+++ frontend-react/src/components/Home.tsx
@@ -6,18 +6,21 @@ import './styles.css';
 function Home() {
     const [listComment, setComments] = useState<Comment[]>([]);
     const [inputComment, setInputComment] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         getComments();
-      }, [])
+    }, [])
 
     const handleAudio = async (e: any) => {
+        setLoading(true);
         const audio = new Audio(`${apiReturnFile}/pronunciation/${e.target.id}`);
         let playPromise = audio.play();
-        
+
         playPromise.then(() => {
-            console.log('Sucesso');
+            setLoading(false);
         }).catch(err => {
+            setLoading(false);
             alert(err)
         });
     }
@@ -27,9 +30,13 @@ function Home() {
     }
 
     const getComments = async () => {
+        setLoading(true);
+        
         await api.get('/comments').then((resp) => {
+            setLoading(false);
             setComments(resp.data);
         }).catch((err) => {
+            setLoading(false);
             alert(err.message);
         });
     }
@@ -37,37 +44,47 @@ function Home() {
     const postComment = async () => {
         if (inputComment === '') return;
 
+        setLoading(true);
         const comment: Comment = { description: inputComment };
-        
+
         await api.post('/comments', comment).then(() => {
             getComments();
+            setLoading(false);
             setInputComment('');
         }).catch(() => {
-
+            setLoading(false);
         });
     }
 
     return (
-        <div className={'app-container'}>
-            <div className={'left-container'}>
-                <h3>Comentario</h3>
-                <textarea value={inputComment} onChange={handleChangeInput} ></textarea>
-                <button className={'button-cadastrar'} type="button" onClick={postComment}>Cadastrar</button>
+        <>
+            {loading ? <div className={'loading-container'}>
+                <span className={'span-loading'}>Carregando ...</span>
+            </div> : null}
+            
+            <div className={'app-container'}>
+                <div className={'left-container'}>
+                    <h3>Comentario</h3>
+                    <textarea value={inputComment} onChange={handleChangeInput} ></textarea>
+                    <button className={'button-post'} type="button" onClick={postComment}>Cadastrar</button>
+                </div>
+                <hr />
+                <div className={'right-container'}>
+                    <h3>Comentários</h3>
+                    {listComment.length > 0 && listComment.map((comment: Comment) => (
+                        <div key={comment.id} className={'item-conatiner'}>
+                            <p className={'text-comment'}>{comment.description}</p>
+                            <div className={'button-container'}>
+                                <button className={'button-audio'} type="button"
+                                    onClick={handleAudio} id={(comment.id).toString()}>
+                                    Ouvir
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className={'right-container'}>
-                <h3>Comentários</h3>
-                {listComment.length > 0 && listComment.map((comment: Comment) => (
-                    <div key={comment.id} className={'item-conatiner'}>
-                        <p className={'text-comment'}>{comment.description}</p>
-                        <button className={'button-comment'} type="button" 
-                            onClick={handleAudio} id={(comment.id).toString()}>
-                            Ouvir
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-
+        </>
     );
 }
 
